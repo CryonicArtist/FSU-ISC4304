9a31a2982dfe0a315161606dfb532b104c7724a8
CryonicAsian <soumilg12@gmail.com>
2026-01-13 13:46:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-a = 0
-b = 10
-c = 5
-while a < b:
-    a = a + 1
-    if a < c:
-        print(a, end=' ')
-    else:
-        print(a*a, end=' ')
-print("done")
+a = [0,1,2,3,4,5,6,7,8,9]
+b=a[0]
+c=a[-1]
+d=a[2:4]
+e=a[3:-2]
+print(b)
+print(c)
+print(d)
+print(e)
