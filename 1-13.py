7321b2732056069e866a2e0fba2f5488d071d56f
CryonicAsian <soumilg12@gmail.com>
2026-01-13 14:03:10 -0500

    strings

---
+++ 1-13.py
@@ -0,0 +1,11 @@
+a="the quick fox jumps over the lazy dog"
+b=a[-1]
+e=a[3:-8]
+f=list(a)
+g = a.split()
+h = a.upper()
+print(b)
+print(e)
+print(f)
+print(g)
+print(h)
--- test.py
+++
@@ -1,9 +0,0 @@
-a = [0,1,2,3,4,5,6,7,8,9]
-b=a[0]
-c=a[-1]
-d=a[2:4]
-e=a[3:-2]
-print(b)
-print(c)
-print(d)
-print(e)
